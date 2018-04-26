d280a0d25fe3c8c47409f106ba9bff0a2bf43f9a
Jochen Manz <jochen.manz@gmx.de>
2018-04-26 16:15:03 +0200

    add phar version and optimize binary operator spacing

--- README.md
+++ README.md
@@ -12,4 +12,4 @@ After that, decide which configuration you want to use, currenty the following c
 
 Add a `.php_cs.dist` to the root folder of your project with the required chosen configuration applied. A good example can be found in this project root folder.
 
-Finally add `.php_cs.cache`  to project specific .gitignore file  and run php-cs-fixer with the following command: `vendor/bin/php-cs-fixer fix -vvv`
+Finally add `.php_cs.cache`  to project specific .gitignore file  and run php-cs-fixer with the following command: `vendor/bin/php-cs-fixer.phar fix -vvv`
--- src/PHP71.php
+++ src/PHP71.php
@@ -23,35 +23,35 @@ final class PHP71 extends Config
     public function getRules()
     {
         return [
-            '@PSR2'                      => true,
-            '@Symfony'                   => true,
-            '@DoctrineAnnotation'        => true,
-            'no_useless_else'            => true,
-            'no_useless_return'          => true,
-            'ordered_class_elements'     => true,
-            'ordered_imports'            => true,
-            'phpdoc_order'               => true,
-            'ternary_to_null_coalescing' => true,
-            'visibility_required'        => [
-                'const',
-                'property',
-                'method',
-            ],
-            'phpdoc_summary'             => false,
-            'phpdoc_to_comment'          => false,
-            'yoda_style'                 => false,
-            'phpdoc_var_without_name'    => false,
-            'concat_space'               => [
+            '@PSR2'                                     => true,
+            '@Symfony'                                  => true,
+            '@DoctrineAnnotation'                       => true,
+            'no_useless_else'                           => true,
+            'no_useless_return'                         => true,
+            'ordered_class_elements'                    => true,
+            'ordered_imports'                           => true,
+            'phpdoc_order'                              => true,
+            'ternary_to_null_coalescing'                => true,
+            'phpdoc_summary'                            => false,
+            'phpdoc_to_comment'                         => false,
+            'yoda_style'                                => false,
+            'phpdoc_var_without_name'                   => false,
+            'no_multiline_whitespace_before_semicolons' => true,
+            'concat_space'                              => [
                 'spacing' => 'one',
             ],
             'array_syntax' => [
                 'syntax' => 'short',
             ],
-            'no_multiline_whitespace_before_semicolons' => true,
-            'binary_operator_spaces'                    => [
+            'visibility_required' => [
+                'const',
+                'property',
+                'method',
+            ],
+            'binary_operator_spaces' => [
                 'operators' => [
-                    '=>' => 'align',
-                    '='  => 'align',
+                    '=>' => 'align_single_space_minimal',
+                    '='  => 'align_single_space_minimal',
                 ],
             ],
         ];
