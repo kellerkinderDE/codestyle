422bc0a108185793326093d72e5e9a66ca59b249
Jochen Manz <j.manz@kellerkinder.de>
2020-04-01 17:52:19 +0200

    [CTRL-1087] move configuration from 7.4 to 7.3

--- src/PHP74.php
+++ src/PHP74.php
@@ -9,18 +9,6 @@ final class PHP74 extends DefaultRules
 	 */
     public static function getRules()
     {
-		return array_merge(
-			PHP73::getRules(),
-			[
-				'global_namespace_import' => [
-					'import_classes'   => true,
-					'import_constants' => true,
-					'import_functions' => true,
-				],
-				'heredoc_indentation' => true,
-				'fully_qualified_strict_types' => true,
-				'nullable_type_declaration_for_default_null_value' => true
-			]
-		);
+		return PHP73::getRules();
     }
 }
